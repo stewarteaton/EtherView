fe8f0db48c2bec82ac267f8a27983a080ddc51f9
stewarteaton <stewarteaton05@gmail.com>
2021-09-09 10:53:02 -0400

    update UI & add miners list

--- app.py
+++ app.py
@@ -1,20 +1,46 @@
 from flask import Flask, render_template
-import config, ccxt
+import config, ccxt, time
 from web3 import Web3
 import ccxt
 
 
 app = Flask(__name__)
 
+# Web3 using Infura API 
 w3 = Web3(Web3.HTTPProvider(config.INFURA_URL))
+
+def get_eth_price():
+    # connect to binance api
+    binance = ccxt.binance()
+    ether_price = binance.fetch_ticker('ETH/USDC')    
+
+    return ether_price
 
 @app.route("/")
 def index():
-    # connect to binance api
-    binance = ccxt.binance()
-    ether_price = binance.fetch_ticker('ETH/USDC')
 
-    return render_template('index.html', ether_price=ether_price)
+    eth = w3.eth
+    ether_price = get_eth_price()
+
+    latest_blocks = []
+    for block_number in range(w3.eth.block_number, w3.eth.block_number-10, -1):
+        block = w3.eth.get_block(block_number)
+        latest_blocks.append(block)
+
+    latest_transactions = []
+    for tx in latest_blocks[-1]['transactions'][-10:]:
+        transaction = w3.eth.get_transaction(tx)
+        latest_transactions.append(transaction)
+
+    current_time = time.time()
+
+    return render_template('index.html', 
+        eth = eth,
+        ether_price=ether_price, 
+        latest_blocks=latest_blocks, 
+        latest_transactions=latest_transactions,
+        current_time=current_time,
+        miners = config.MINERS)
 
 @app.route("/address/<addy>")
 def address(addy):
